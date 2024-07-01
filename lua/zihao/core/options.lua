0e5ed42b3a0cfac8505c6d5f582d251cdca39105
Zi Hao LAM <lamzihao98@gmail.com>
2024-07-01 08:19:31 +0800

    add copilot plugin

--- lua/zihao/core/options.lua
+++ lua/zihao/core/options.lua
@@ -6,8 +6,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- 2 spaces for indent width
+opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
